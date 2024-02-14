01cf8a3173bfc5b3eb53d10ced3f249ac4186171
RahulSurRoy244 <159292484+RahulSurRoy244@users.noreply.github.com>
2024-02-14 09:51:19 +0530

    Update script.js

--- Weather App/script.js
+++ Weather App/script.js
@@ -26,7 +26,7 @@ btn.addEventListener('click', function()
 
 
     city.innerHTML=`Weather of <span>${nameval}<span>`
-    temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
+    temp.innerHTML = `Temperature: <span>${ convertion(tempature)}°C</span>`
     description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
     wind.innerHTML = `Wind Speed: <span>${wndspeed} km/h<span>`
 
